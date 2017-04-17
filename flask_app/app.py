8438df5f7ce62aba843551fcad73bd1ebe2c75f3
ad1m <alieberman3@mail.gatech.edu>
2017-04-17 12:26:25 -0400

    New files

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,7 @@
 __author__ = 'Adamlieberman'
 from flask import Flask, render_template, request
-from scraper import scrape_icd9
+from scraper import scrape_icd9, cliner_response
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,12 +17,13 @@ def input_page_post():
 
     #Obtain the clinical note
     clinical_note = request.form.get('clinical_note',type=str)
+    cliner_note = cliner_response(clinical_note)
     codes = [250.01, 250.02, 250.03]
     description = scrape_icd9(codes)
     #Process the clinical note
     clinical_note = clinical_note.lower()
-    t = 'h<font color="red">aaaa</font>gggg'
-    return render_template('results.html',note=clinical_note, code=codes,t=t,description=description)
+    #t = 'h<font color="red">aaaa</font>gggg'
+    return render_template('results.html',note=cliner_note, code=codes,description=description)
 
 
 if __name__ == "__main__":
--- flask_app/scraper.py
+++ flask_app/scraper.py
@@ -1,7 +1,7 @@
 __author__ = 'Adamlieberman'
 from bs4 import BeautifulSoup
+from mechanize import Browser
 import requests
-
 
 '''
 Scrape ICD-9 descriptions for a given ICD-9 code
@@ -23,7 +23,25 @@ def scrape_icd9(codes):
                 break
             count = count + 1
     return all_descriptions
+
+
+def cliner_response(note):
+    browser = Browser()
+    browser.open('http://text-machine.cs.uml.edu/cliner/demo/cgi-bin/cliner_demo.cgi/')
+    browser.select_form(nr=0)
+    browser['user_input'] = note
+    response = browser.submit()
+    content = response.read()
+    soup = BeautifulSoup(content,"lxml")
+    cleaned_response = soup.find_all('p')[0]
+    return cleaned_response
+   # print type(content)
+
+
 if __name__=="__main__":
-    codes = [250.01,250.02,250.03]
-    description = scrape_icd9(codes)
-    print description
+    note = "Patient was admitted for a routine surgery on Thursday.  He had complications overnight and was transferred to the ICU."
+    cleaned_response = cliner_response(note)
+    print cleaned_response
+    #codes = [250.01,250.02,250.03]
+    #description = scrape_icd9(codes)
+    #print description
