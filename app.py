c94d7037047ab27abf5d6a643ff838bb97e44b29
Ravish Chawla <ravishchawla@lawn-128-61-120-246.lawn.gatech.edu>
2017-04-19 14:17:32 -0400

    Heroku changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 __author__ = 'Adamlieberman'
 from flask import Flask, render_template, request
 from scraper import scrape_icd9, cliner_response
+import os;
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def input_page_post():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000));
+    app.run(host='0.0.0.0', port=port, debug=True);
